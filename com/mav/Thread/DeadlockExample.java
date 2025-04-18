9433f7db4e6228ee6d2666a56c75aa0d9fa37592
Sunit017 <kumarsunit017@gmail.com>
2025-04-18 14:20:34 +0530

    notes and multithreading

---
+++ com/mav/Thread/DeadlockExample.java
@@ -0,0 +1,35 @@
+package com.mav.Thread;
+
+public class DeadlockExample {
+
+    static final Object Lock1 = new Object();
+    static final Object Lock2 = new Object();   
+
+    public static void main(String[] args) {
+        Thread t1 = new Thread(() -> {
+            synchronized (Lock1) {
+                System.out.println("Thread 1: Holding Lock1...");
+                try { Thread.sleep(100); } catch (Exception e) {}
+                System.out.println("Thread 1: Waiting for Lock2...");
+                synchronized (Lock2) {
+                    System.out.println("Thread 1: Got Lock2!");
+                }
+            }
+        });
+
+        Thread t2 = new Thread(() -> {
+            synchronized (Lock2) {
+                System.out.println("Thread 2: Holding Lock2...");
+                try { Thread.sleep(100); } catch (Exception e) {}
+                System.out.println("Thread 2: Waiting for Lock1...");
+                synchronized (Lock1) {
+                    System.out.println("Thread 2: Got Lock1!");
+                }
+            }
+        });
+
+        t1.start();
+        t2.start();
+    }
+}
+
